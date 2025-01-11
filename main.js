9a0561df305149e2328a55bc7dc9692378ab6dc9
sebastian-92 <140918855+sebastian-92@users.noreply.github.com>
2025-01-11 17:31:18 -0500

    fix

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ if (params.has("path")) {
   let path = params.get("path");
   // Get HTML and metadata for article on English Wikipedia
   let base = "https://api.wikimedia.org/core/v1/wikipedia/en/page/";
-  let url = base + "/" + path + "/with_html";
+  let url = base + path + "/with_html";
   fetch(url)
     .then((res) => {
       return res.json();
     })
     .then((res) => {
-      res.html
+      html = res.html
         .replaceAll("/n", "<br>")
         .replaceAll("en.wikipedia.org", "credipedia.org");
       document.getElementById("content").innerHTML = res.html;
