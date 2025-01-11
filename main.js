85a14ec97b1ce4a764e312ca5abf1b46b86d0b3d
sebastian-92 <140918855+sebastian-92@users.noreply.github.com>
2025-01-11 17:14:35 -0500

    initial changes

---
+++ main.js
@@ -0,0 +1,19 @@
+const params = new URLSearchParams(document.location.search);
+if (params.has("path")) {
+  let path = params.get("path");
+  // Get HTML and metadata for article on English Wikipedia
+  let base = "https://api.wikimedia.org/core/v1/wikipedia/en/page/";
+  let url = base + "/" + path + "/with_html";
+  let response = await fetch(url);
+  response
+    .json()
+    .then((res) => {
+      res.html
+        .replaceAll("/n", "<br>")
+        .replaceAll("en.wikipedia.org", "credipedia.org");
+      document.getElementById("content").innerHTML = res.html;
+      document.getElementById("title").innerHTML = res.title;
+      console.log(res);
+    })
+    .catch(console.error);
+}
